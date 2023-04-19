774011f138805ce37c8248c155440209fa47e834
Le Huy <103082623+lgh2003@users.noreply.github.com>
2023-04-19 23:13:14 +0700

    Update duyetungvien.php

--- src/Views/admin/duyetungvien.php
+++ src/Views/admin/duyetungvien.php
@@ -38,114 +38,69 @@ require("check.php");
             position: sticky;
         }
     </style>
-    <div id="main">
+    <div id="main" class="h-100">
         <?php
         require_once("./app/navbar.php");
         ?>
-
         <div id="content">
-            <div class="container container-content">
+            <div class="container-fluid container-content vh-100">
                 <div class="row">
-                    <div class="col-sm-12 col-duyetungvien">
-                        <div class="col-sm-12 format-text-heading">
-                            <h4>DUYỆT ỨNG VIÊN</h4>
+                <div class="d-sm-flex flex-sm-row">
+                        <div class="col-sm-4 col-duyetungvien">
+                            <div class="col-sm-12 format-text-heading">
+                                <h4>ĐƠN ỨNG TUYỂN</h4>
+                            </div>
+                            <div class="table-responsive m-1">
+                                <table id="duyetungvien" class="table align-middle mb-0 bg-black format-table">
+                                    <thead class="format-thead-background bg-success text-white">
+                                        <tr class="format-text">
+                                            <th></th>
+                                            <th>Tên</th>
+                                            <th>Công việc</th>
+                                            <th>Chi tiết</th>
+                                            <th>Duyệt</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
                         </div>
-                        <div class="table-responsive">
-                            <table id="duyetungvien" class="table align-middle mb-0 bg-white format-table">
-                                <thead class="format-thead-background">
-                                    <tr class="format-text">
-                                        <th></th>
-                                        <th>Tên</th>
-                                        <th>Công việc</th>
-                                        <th>Chi tiết</th>
-                                        <th>Duyệt</th>
-                                    </tr>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
+                        <div class="col-sm-4 col-duyetungvien">
+                            <div class="col-sm-12 format-text-heading">
+                                <h4>LỜI MỜI LÀM VIỆC</h4>
+                            </div>
+                            <div class="table-responsive m-1">
+                                <table id="yeucautuyendung" class="table align-middle mb-0 bg-white format-table">
+                                    <thead class="format-thead-background bg-success text-white">
+                                        <tr class="format-text">
+                                            <th></th>
+                                            <th>Tên</th>
+                                            <th>Công việc</th>
+                                            <th>Chi tiết</th>
+                                            <th>Trạng thái</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-duyetungvien">
-                        <div class="col-sm-12 format-text-heading">
-                            <h4>ỨNG VIÊN ĐƯỢC GỬI YÊU CẦU TUYỂN DỤNG</h4>
-                        </div>
-                        <div class="table-responsive">
-                            <table id="yeucautuyendung" class="table align-middle mb-0 bg-white format-table">
-                                <thead class="format-thead-background">
-                                    <tr class="format-text">
-                                        <th></th>
-                                        <th>Tên</th>
-                                        <th>Công việc</th>
-                                        <th>Chi tiết</th>
-                                        <th>Trạng thái</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <!-- <tr class="format-text">
-                                        <td>
-                                            <div class="d-flex align-items-center">
-                                                <img src="../../../src/assets/Images/18-F.png" alt="" style="width: 45px; height: 45px" class="rounded-circle" />
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <p class="fw-normal mb-1">Taylor Swift</p>
-                                        </td>
-                                        <td>Nhạc sĩ</td>
-                                        <td class="format-td">
-                                            <div class="format-td-hoso">
-                                                <a class="format-hoso" role="button" href="#" class="btn btn-link  btn-rounded">
-                                                    Xem hồ sơ
-                                                </a>
-                                            </div>
-
-                                        </td>
-                                        <td>
-                                            <span class="format-text-dadongy"> Đã đống ý</span>
-                                        </td>
-                                    </tr>
-                                    <tr class="format-text">
-                                        <td>
-                                            <div class="d-flex align-items-center">
-                                                <img src="../../../src/assets/Images/18-F.png" alt="" style="width: 45px; height: 45px" class="rounded-circle" />
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <p class="fw-normal mb-1">Taylor Swift</p>
-                                        </td>
-                                        <td>Nhạc sĩ</td>
-                                        <td class="format-td">
-                                            <div class="format-td-hoso">
-                                                <a class="format-hoso" role="button" href="#" class="btn btn-link  btn-rounded">
-                                                    Xem hồ sơ
-                                                </a>
-                                            </div>
-
-                                        </td>
-                                        <td>
-                                            <span class="format-text-datuchoi"> Đã từ chối </span>
-                                        </td>
-                                    </tr> -->
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-ungviendaduyet">
-                        <div class="col-sm-12 format-text-heading">
-                            <h4>ỨNG VIÊN ĐÃ DUYỆT</h4>
-                        </div>
-                        <div class="table-responsive">
-                            <table id="daduyet" class="table align-middle mb-0 bg-white format-table">
-                                <thead class="format-thead-background">
-                                    <tr class="format-text">
-                                        <th></th>
-                                        <th>Tên</th>
-                                        <th>Công việc</th>
-                                        <th>Chi tiết</th>
-
-                                    </tr>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
+                        <div class="col-sm-4 col-ungviendaduyet">
+                            <div class="col-sm-12 format-text-heading">
+                                <h4>ĐƠN ĐÃ DUYỆT</h4>
+                            </div>
+                            <div class="table-responsive m-1">
+                                <table id="daduyet" class="table align-middle mb-0 bg-white format-table">
+                                    <thead class="format-thead-background bg-success text-white">
+                                        <tr class="format-text">
+                                            <th></th>
+                                            <th>Tên</th>
+                                            <th>Công việc</th>
+                                            <th>Chi tiết</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
