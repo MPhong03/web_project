5baf0ba69140c4894fcd8092f29501435388c405
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-19 20:48:22 +0700

    hehehehehehe

--- src/Views/layouts/app/content2.php
+++ src/Views/layouts/app/content2.php
@@ -1,4 +1,75 @@
 <div class="content-3" id="content-3">
+  <div class="brands" id="brands">
+    <div class="container pt-0">
+      <div class="row">
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="box-find" class="box-find mt-4" role="search">
+    <form action="">
+      <div class="container pt-3 pb-5" id="jobsearch">
+        <div class="box-form">
+          <div class="mb-2 form-group enter-text">
+            <i class="bi bi-search"></i>
+            <input id="myInput" type="text" class="form-input form-control" placeholder="Nhập tên công ty, vị trí, từ khóa....">
+          </div>
+
+          <div class="mb-2 form-group enter-text">
+            <i class="bi bi-map"></i>
+            <!-- <input type="text" class="form-control" placeholder="Vị trí">\ -->
+            <select id="myInput1" name="location-search" class="form-select form-control col-lg-2"
+              aria-label="Default select example">
+              <option selected value="0">Tất cả địa điểm</option>
+              <option value="1">Hà Nội</option>
+              <option value="2">Hồ Chí Minh</option>
+              <option value="3">Nước ngoài</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="rest-form mb-3">
+          <a href=""></a>
+          <i class="bi bi-arrow-counterclockwise"></i>
+          <input class="reset border-0 bg-white" type="reset" value="Reset">
+        </div>
+
+
+        <div class="">
+          <a href="#showJob" class="text-light">
+            <button onclick="searchFilterFromUser()" name="search-main" value="1"
+              class="btn btn-dark w-100 form-control-plaintext text-center btn-timviec" tabindex="-1" role="button" type="submit">
+              Tìm Việc Ngay
+            </button>
+          </a>
+        </div>
+      </div>
+    </form>
+  </div>
+
   <div class="container container-content-3">
     <div class="row">
       <div class="col-12">
@@ -7,10 +78,14 @@
           <div class="filter-dropdown">
             <div class="filter-dropdown-container">
 
-              <button class="btn-filter-dropdown" id="btn-filterdropdown" aria-haspopup="true" aria-expanded="false" aria-controls="filter-menu">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-briefcase-fill" viewBox="0 0 16 16">
-                  <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
-                  <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
+              <button class="btn-filter-dropdown" id="btn-filterdropdown" aria-haspopup="true" aria-expanded="false"
+                aria-controls="filter-menu">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor"
+                  class="bi bi-briefcase-fill" viewBox="0 0 16 16">
+                  <path
+                    d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
+                  <path
+                    d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
                 </svg>
                 <div class="filter-dropdown-lable">
                   Tất cả lĩnh vực
@@ -21,7 +96,8 @@
               </button>
 
               <ul class="filter-dropdownlist filter-dropdownlist-linhvuc" id="filter-menu">
-                <input class="form-control myInputSearch" id="myInput2" type="text" autocomplete="off" placeholder="Search..">
+                <input class="form-control myInputSearch" id="myInput2" type="text" autocomplete="off"
+                  placeholder="Search..">
                 <li class="filter-dropdownlistItem">
                   <div class="filter-dropdownlistitemLink">Tất cả</div>
                 </li>
@@ -34,9 +110,12 @@
           </div>
           <div class="filter-dropdown">
             <div class="filter-dropdown-container">
-              <button class="btn-filter-dropdown" id="btn-filterdropdown1" aria-haspopup="true" aria-expanded="false" aria-controls="filter-menu">
+              <button class="btn-filter-dropdown" id="btn-filterdropdown1" aria-haspopup="true" aria-expanded="false"
+                aria-controls="filter-menu">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 25.595 25.595">
-                  <path id="ic_euro_symbol_24px" d="M20.063,25.04a9.214,9.214,0,0,1-8.19-4.977h8.19V17.22H10.934a8.944,8.944,0,0,1,0-2.844h9.129V11.532h-8.19A9.225,9.225,0,0,1,26.078,8.787L28.6,6.27a12.779,12.779,0,0,0-20.59,5.261H3v2.844H7.351a11.892,11.892,0,0,0,0,2.844H3v2.844H8.005A12.779,12.779,0,0,0,28.6,25.325l-2.531-2.517A9.108,9.108,0,0,1,20.063,25.04Z" transform="translate(-3 -3)" />
+                  <path id="ic_euro_symbol_24px"
+                    d="M20.063,25.04a9.214,9.214,0,0,1-8.19-4.977h8.19V17.22H10.934a8.944,8.944,0,0,1,0-2.844h9.129V11.532h-8.19A9.225,9.225,0,0,1,26.078,8.787L28.6,6.27a12.779,12.779,0,0,0-20.59,5.261H3v2.844H7.351a11.892,11.892,0,0,0,0,2.844H3v2.844H8.005A12.779,12.779,0,0,0,28.6,25.325l-2.531-2.517A9.108,9.108,0,0,1,20.063,25.04Z"
+                    transform="translate(-3 -3)" />
                 </svg>
 
                 <div class="filter-dropdown-lable">
@@ -47,7 +126,8 @@
                 </div>
               </button>
               <ul class="filter-dropdownlist filter-dropdownlist-mucluong" id="filter-menu1">
-                <input class="form-control myInputSearch" id="myInput3" autocomplete="off" type="text" placeholder="Search..">
+                <input class="form-control myInputSearch" id="myInput3" autocomplete="off" type="text"
+                  placeholder="Search..">
                 <li class="filter-dropdownlistItem">
                   <div class="filter-dropdownlistitemLink">Tất cả</div>
                 </li>
@@ -59,8 +139,10 @@
           </div>
           <div class="filter-dropdown">
             <div class="filter-dropdown-container">
-              <button class="btn-filter-dropdown" id="btn-filterdropdown2" aria-haspopup="true" aria-expanded="false" aria-controls="filter-menu">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+              <button class="btn-filter-dropdown" id="btn-filterdropdown2" aria-haspopup="true" aria-expanded="false"
+                aria-controls="filter-menu">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-clock"
+                  viewBox="0 0 16 16">
                   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
                   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
                 </svg>
@@ -73,7 +155,8 @@
                 </div>
               </button>
               <ul class="filter-dropdownlist filter-dropdownlist-hinhthuc" id="filter-menu2">
-                <input class="form-control myInputSearch" id="myInput4" autocomplete="off" type="text" placeholder="Search..">
+                <input class="form-control myInputSearch" id="myInput4" autocomplete="off" type="text"
+                  placeholder="Search..">
                 <li class="filter-dropdownlistItem">
                   <div class="filter-dropdownlistitemLink">Tất cả</div>
                 </li>
@@ -85,8 +168,10 @@
           </div>
           <div class="filter-dropdown">
             <div class="filter-dropdown-container">
-              <button class="btn-filter-dropdown" id="btn-filterdropdown3" aria-haspopup="true" aria-expanded="false" aria-controls="filter-menu">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+              <button class="btn-filter-dropdown" id="btn-filterdropdown3" aria-haspopup="true" aria-expanded="false"
+                aria-controls="filter-menu">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor"
+                  class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
                 </svg>
 
@@ -98,7 +183,8 @@
                 </div>
               </button>
               <ul class="filter-dropdownlist filter-dropdownlist-diadiem" id="filter-menu3">
-                <input class="form-control myInputSearch" id="myInput5" autocomplete="off" type="text" placeholder="Search..">
+                <input class="form-control myInputSearch" id="myInput5" autocomplete="off" type="text"
+                  placeholder="Search..">
                 <li class="filter-dropdownlistItem">
                   <div class="filter-dropdownlistitemLink">Tất cả</div>
                 </li>
@@ -109,9 +195,12 @@
             </div>
           </div>
           <div class="filter-dropdown-icon">
-            <button class="btn-filter-dropdown" id="btn-filterdropdown4" aria-haspopup="true" aria-expanded="false" aria-controls="filter-menu4" onclick="btnFilter(getFilterFromUser)">
-              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
-                <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+            <button class="btn-filter-dropdown" id="btn-filterdropdown4" aria-haspopup="true" aria-expanded="false"
+              aria-controls="filter-menu4" onclick="btnFilter(getFilterFromUser)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-filter"
+                viewBox="0 0 16 16">
+                <path
+                  d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
               </svg>
             </button>
           </div>
