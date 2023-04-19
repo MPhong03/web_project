81e8351ad0631289339fd28a611237a078b85ae5
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-19 23:32:27 +0700

    hehehe

--- src/Views/layouts/app/content1.php
+++ src/Views/layouts/app/content1.php
@@ -1,15 +1,14 @@
 <div class="content-1">
-    <div class="bg-image shadow-1-strong rounded search-content">
-        <div class="mask">
-            <div class="d-flex justify-content-center h-100">
-                <div class="container container-content-1">
-                    <div class="row" style="margin-top: 20vh;">
-                        <div
-                            class="col-lg-6 d-flex flex-column justify-content-center pt-4 pb-4 pt-lg-0 order-2 order-lg-1">
+    <div id="slider">
+        <div id="banner" class="d-flex align-items-center justify-content-center">
+            <div class="container">
+                <div class="row">
+                    <div class="row">
+                        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pb-4 pt-lg-0 order-2 order-lg-1">
                             <h1>Khám phá sự nghiệp mơ ước của bạn</h1>
                             <h2>Bắt đầu tìm kiếm với chúng tôi!</h2>
                             <div class="d-flex justify-content-center justify-content-lg-start">
-                                <a href="#" class="btn-start form-control-plaintext nav-link border-0 text-center"
+                                <a href="#content-3" class="btn-start form-control-plaintext nav-link border-0 text-center"
                                     style="width: 180px; padding: 15px 0;">Bắt đầu ngay</a>
                             </div>
                         </div>
@@ -19,8 +18,8 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
-</div>
 </div>
