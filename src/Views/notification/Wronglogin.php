baeb4f203b1d7d5e1b215de8df0a69fc2ec00cd5
Le Huy <103082623+lgh2003@users.noreply.github.com>
2023-04-20 08:33:36 +0700

    Update Wronglogin.php

--- src/Views/notification/Wronglogin.php
+++ src/Views/notification/Wronglogin.php
@@ -2,7 +2,7 @@
     class Wronglogin{
         function printError($display,$content){
             // echo "<h3 style='diplay:$display;position: relative;z-index:100;' class='text-center text-uppercase align-center text-white'>$content</h3>";
-            echo "<div style='font-size:initial;diplay:$display;position:fixed;bottom:90%;right:45%;z-index:100;opacity:1;' class='m-0 toast' >
+            echo "<div style='font-size:initial;diplay:$display;position:fixed;bottom:85%;right:50%;z-index:100;opacity:1;' class='m-0 toast' >
             <div class='toast-body bg-dark text-center text-white'>Thông báo</div>
             <div class='toast-body bg-dark text-center text-white'>
                 $content
