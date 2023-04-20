ac7cfffd09a531404912e06f38fa9b4fd1fb5314
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-20 11:40:24 +0700

    đã có footer vui lòng check

--- src/Views/layouts/app/footer.php
+++ src/Views/layouts/app/footer.php
@@ -1,27 +1,41 @@
 <footer id="footer" class="text-center text-lg-start text-white">
   <!-- Section: Social media -->
-  <section class="container-footer d-flex justify-content-center justify-content-lg-between pt-4 px-4">
+  <section
+    class="container-footer d-flex justify-content-center justify-content-lg-between align-items-center pt-4 px-4">
     <!-- Left -->
-    <div class="me-5 d-none d-lg-block">
-      <span style="font: initial">Đồ án Website tìm kiếm việc làm</span>
+    <div class="me-5 d-lg-block d-none">
+      <img class="me-2" src="../../../src/assets/Images/logo.png" alt="logo" height="80" width="80">
+      <!-- <span style="font-size: 24px">SEEKER</span> -->
     </div>
-    <!-- Left -->
+
+    <!-- Middle -->
+    <div>
+      <p class="m-0">© 2014 - 2023 Công ty cổ phần Seeker Việt Nam <br>
+        Trụ sở chính: 19 Nguyễn Hữu Thọ, Phường Tân Phong, Quận 7, TP. Hồ Chí Minh
+      </p>
+    </div>
 
     <!-- Right -->
     <div>
-        <h5 class="text-center text-white">Thành viên</h5>
-      <a style="text-decoration:none;" href="" class="me-4 text-reset" >
-        <i class="">PHẠM DUY KHOA</i>
-      </a>
-      <a style="text-decoration:none;" href="" class="me-4 text-reset">
-        <i class="">DƯƠNG HẢI ĐĂNG</i>
-      </a>
-      <a style="text-decoration:none;" href="" class="me-4 text-reset">
-        <i class="">NGUYỄN NGỌC ÁI ĐAN</i>
-      </a>
-      <a style="text-decoration:none;" href="" class="me-4 text-reset">
-        <i class="">TRẦN GIA BẢO</i>
-      </a>
+      <h5 class="text-center text-white me-3 d-lg-block d-none">KẾT NỐI VỚI SEEKER</h5>
+      <div class="d-flex justify-content-center align-items-center d-lg-block d-none">
+        <a style="text-decoration:none;" href="" class="me-4 text-reset">
+          <!-- <i class="ti-facebook">PHẠM DUY KHOA</i> -->
+          <i class="text-white fa fa-brands fa-facebook"></i>
+        </a>
+        <a style="text-decoration:none;" href="" class="me-4 text-reset">
+          <!-- <i class="">DƯƠNG HẢI ĐĂNG</i> -->
+          <i class="text-white fa fa-brands fa-instagram"></i>
+        </a>
+        <a style="text-decoration:none;" href="" class="me-4 text-reset">
+          <!-- <i class="">NGUYỄN NGỌC ÁI ĐAN</i> -->
+          <i class="text-white fa fa-brands fa-twitter"></i>
+        </a>
+        <a style="text-decoration:none;" href="" class="me-4 text-reset">
+          <!-- <i class="">TRẦN GIA BẢO</i> -->
+          <i class="text-white fa fa-brands fa-linkedin"></i>
+        </a>
+      </div>
     </div>
   </section>
 </footer>
