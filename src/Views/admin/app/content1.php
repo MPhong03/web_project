ea79560ba6baacaafaf13e31a64319ab63ff47d4
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-20 11:39:10 +0700

    abc

--- src/Views/admin/app/content1.php
+++ src/Views/admin/app/content1.php
@@ -1,14 +1,16 @@
 <div class="content-1">
-    <div id="slider">
+    <div id="slider" class="slider-admin" style="background-image: linear-gradient(180deg, #b0d4b8, rgba(176, 212, 184, 0.7)), url('../../../src/assets/Images/bg_ntd.png'); background-size: cover;">
         <div id="banner" class="d-flex align-items-center justify-content-center">
             <div class="container">
                 <div class="row">
                     <div class="row">
-                        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pb-4 pt-lg-0 order-2 order-lg-1">
-                            <h1>Khám phá nguồn nhân lực mơ ước của bạn</h1>
+                        <div
+                            class="col-lg-6 d-flex flex-column justify-content-center pt-4 pb-4 pt-lg-0 order-2 order-lg-1">
+                            <h1>Khám phá sự nghiệp mơ ước của bạn</h1>
                             <h2>Bắt đầu tìm kiếm với chúng tôi!</h2>
                             <div class="d-flex justify-content-center justify-content-lg-start">
-                                <a href="#content-3" class="btn-start form-control-plaintext nav-link border-0 text-center"
+                                <a href="#content-3"
+                                    class="btn-start form-control-plaintext nav-link border-0 text-center"
                                     style="width: 180px; padding: 15px 0;">Bắt đầu ngay</a>
                             </div>
                         </div>
