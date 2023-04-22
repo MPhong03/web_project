37524c365a191d14145b2d54a00d4fdc1010ef0c
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-22 13:14:07 +0700

    CL da co gang het suc

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-	session_start();
-	if(isset($_SESSION["user"]) && $_SESSION["user"]=="dadangnhap")
-		Header("Location: ./src/Controllers/C_SignIn.php");
+session_start();
+if(isset($_SESSION["user"]) && $_SESSION["user"]=="dadangnhap")
+	Header("Location: ./src/Controllers/C_SignIn.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,16 +9,14 @@
 <meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Boostrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <!-- Boostrap separate -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <!-- Bootstrap icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-    <!-- Jquery CDN -->
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <!-- Custom.css -->
+	<!-- Boostrap CDN -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+	<!-- Boostrap separate -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+	<!-- Jquery CDN -->
+	<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+	<!-- Custom.css -->
 	<link rel="stylesheet" href="./src/assets/Styles/custom.css">
 	<!-- navbar.css -->
 	<link rel="stylesheet" href="./src/assets/Styles/navbar.css">
@@ -34,7 +32,10 @@
 	<link rel="stylesheet" href="./src/assets/Styles/reponsive.css">
 	<!-- font-awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<title>FindWork</title>
+	<!-- bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+
+	<title>SEEKER</title>
 </head>
 
 <body>
@@ -48,6 +49,7 @@
 		?>
 	</div>
 </body>
+	<script src="./src/assets/Scripts/navbar.js"></script>
 	<script src="./src/assets/Scripts/script.js"></script>
 	<script src="./src/assets/Scripts/content2.js"></script>
 	<script src="./src/assets/Scripts/content3.js"></script>
