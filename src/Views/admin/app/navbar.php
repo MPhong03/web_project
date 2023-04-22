7a3959941656df41cd507bdbf23b192acb444d26
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-22 13:23:44 +0700

    CL da co gang het suc

--- src/Views/admin/app/navbar.php
+++ src/Views/admin/app/navbar.php
@@ -3,31 +3,53 @@
         <div class="container-fluid">
             <!-- <a class="navbar-brand" href="../../../../index.php">FINDWORK</a> -->
             <a class="navbar-brand" href="../../../../index.php">
-				<img src="../../../src/assets/Images/logo.png" alt="logo" height="80" loading="lazy" width="80">
-			</a>
+                <img src="../../../src/assets/Images/logo.png" alt="logo" height="80" loading="lazy" width="80">
+            </a>
 
-            <button class="navbar-toggler" id="btn-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+			<button class="navbar-toggler btn-menu border-0" id="btn-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<!-- <span class="navbar-toggler-icon"></span> -->
+                <i class="bi bi-list"></i>
+			</button>
 
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="../../../../routes.php?dangviec">Đăng việc</a>
-                        
+
                     </li>
                 </ul>
-                
+
                 <ul class="navbar-nav mt-2 mt-lg-0" id="identity_user">
-                    <a class="nav-link active format-name-user">William Shakespeare</a>
+                    <div class="navbar-img-notice filter-dropdown-container-user">
+                        <button class="btn-notice">
+                            <span>
+                                <img class="format-img-notice" src="./Images/ic_notifications_none_24px.png" alt="">
+                            </span>
+                        </button>
+                        <ul class="filter-dropdownlist-notice" id="filter-btn-notice">
+                            <li class="filter-dropdownlistItem">
+                                <span><img class="format-img-user" src="./Images/18-F.png" alt=""></span>
+                                <a href="#" class="filter-dropdownlistitemLink">William Shake đã phản hồi về yêu cầu
+                                    tuyển dụng của bạn</a>
+                            </li>
+                            <li class="filter-dropdownlistItem format-border-top">
+                                <span><img class="format-img-user" src="./Images/18-F.png" alt=""></span>
+                                <a href="#" class="filter-dropdownlistitemLink">John Cena đã phản hồi về yêu cầu tuyển
+                                    dụng của bạn</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
                     <div class="filter-dropdown-container-user">
                         <button class="format-btn-user" id="btn-user" style="background-color:transparent !important;">
-                            <span class="containerBlockicon" style="max-height:40px;max-width:40px;margin-left:5px;margin-top:2px;"><img class="format-img-user" src="./Images/18-F.png" alt=""></span>
+                            <span class="containerBlockicon"
+                                style="max-height:40px;max-width:40px;margin-left:5px;margin-top:2px;"><img
+                                    class="format-img-user" src="./Images/18-F.png" alt=""></span>
                         </button>
                         <ul class="filter-dropdownlist-user" id="filter-btn-user">
                             <li class="filter-dropdownlistItem">
                                 <i class="bi bi-person"></i>
-                                <a href="./capnhatdoanhnghiep.php" class="filter-dropdownlistitemLink">Chỉnh sửa thông tin</a>
+                                <a href="./capnhatdoanhnghiep.php" class="filter-dropdownlistitemLink">Thông tin của bạn</a>
                             </li>
                             <li class="filter-dropdownlistItem ormat-border-top">
                                 <a href="./duyetcongviec.php" class="filter-dropdownlistitemLink">Duyệt công việc</a>
@@ -41,23 +63,7 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="navbar-img-notice filter-dropdown-container-user">
-                        <button class="btn-notice">
-                            <span>
-                                <img class="format-img-notice" src="./Images/ic_notifications_none_24px.png" alt="">
-                            </span>
-                        </button>
-                        <ul class="filter-dropdownlist-notice" id="filter-btn-notice">
-                            <li class="filter-dropdownlistItem">
-                                <span><img class="format-img-user" src="./Images/18-F.png" alt=""></span>
-                                <a href="#" class="filter-dropdownlistitemLink">William Shake đã phản hồi về yêu cầu tuyển dụng của bạn</a>
-                            </li>
-                            <li class="filter-dropdownlistItem format-border-top">
-                                <span><img class="format-img-user" src="./Images/18-F.png" alt=""></span>
-                                <a href="#" class="filter-dropdownlistitemLink">John Cena đã phản hồi về yêu cầu tuyển dụng của bạn</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <a class="nav-link active format-name-user">William Shakespeare</a>
                 </ul>
             </div>
         </div>
