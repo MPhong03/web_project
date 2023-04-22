46913a8c57546c91751900591849ead41fdacceb
PhamNhatQuynh <109475158+PhamNhatQuynh@users.noreply.github.com>
2023-04-22 20:09:35 +0700

    Update homepageUser.php

--- src/Views/user/homepageUser.php
+++ src/Views/user/homepageUser.php
@@ -35,7 +35,7 @@
 	<!-- font-awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
-	<title>Find Work</title>
+	<title>SEEKER</title>
 </head>
 
 <body>
