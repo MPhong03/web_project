85dd8bace946cde1e73b3760b0cb9211e5f32a26
Chi Long <123980542+chilongnee@users.noreply.github.com>
2023-04-22 21:59:05 +0700

    Chi Long da co gang het suc

--- src/Views/layouts/app/content1.php
+++ src/Views/layouts/app/content1.php
@@ -1,14 +1,17 @@
 <div class="content-1">
+    <!-- Slider giới thiệu -->
     <div id="slider">
         <div id="banner" class="d-flex align-items-center justify-content-center">
             <div class="container">
                 <div class="row">
                     <div class="row">
-                        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pb-4 pt-lg-0 order-2 order-lg-1">
+                        <div
+                            class="col-lg-6 d-flex flex-column justify-content-center pt-4 pb-4 pt-lg-0 order-2 order-lg-1">
                             <h1>Khám phá sự nghiệp mơ ước của bạn</h1>
                             <h2>Bắt đầu tìm kiếm với chúng tôi!</h2>
                             <div class="d-flex justify-content-center justify-content-lg-start">
-                                <a href="#content-3" class="btn-start form-control-plaintext nav-link border-0 text-center"
+                                <a href="#content-3"
+                                    class="btn-start form-control-plaintext nav-link border-0 text-center"
                                     style="width: 180px; padding: 15px 0;">Bắt đầu ngay</a>
                             </div>
                         </div>
@@ -22,4 +25,36 @@
             </div>
         </div>
     </div>
+
+    <!-- Brands những công ty đầu tư -->
+    <div class="brands" id="brands">
+        <div class="container pt-0">
+            <div class="row">
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="../../../src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="../../../src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="../../../src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="../../../src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="../../../src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="../../../src/assets/Images/brand-tdt.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
